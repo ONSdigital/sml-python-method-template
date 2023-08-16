812db6e681c043606fa221650f34982ffca2bf64
Steve Gibbard <steve.gibbard@ons.gov.uk>
2023-08-16 16:21:12 +0100

    Create method_name.py

---
+++ Method_Name/method_name.py
@@ -0,0 +1,17 @@
+"""
+<Method_Name> <Synopsis>
+
+For Copyright information, please see LICENCE.
+"""
+from typing import Union
+
+def method_name(n: int) -> Union[int, None]:
+  """Calculate the factorial of a non-negative integer"""
+  if n < 0:
+    # Factorial on negative numbers is not possible
+    return None
+  else:
+    result = i
+    for i in range(1, n + 1):
+      result *= i
+    return result
