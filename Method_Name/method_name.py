478efe3900e94f897e2c36cde42acc97883c7042
Steve Gibbard <88480716+gibbardsteve@users.noreply.github.com>
2023-08-17 11:16:05 +0100

    Clean up liniting in template

--- Method_Name/method_name.py
+++ Method_Name/method_name.py
@@ -5,13 +5,14 @@ For Copyright information, please see LICENCE.
 """
 from typing import Union
 
+
 def method_name(n: int) -> Union[int, None]:
-  """Calculate the factorial of a non-negative integer"""
-  if n < 0:
-    # Factorial on negative numbers is not possible
-    return None
-  else:
-    result = i
-    for i in range(1, n + 1):
-      result *= i
-    return result
+    """Calculate the factorial of a non-negative integer"""
+    if n < 0:
+        # Factorial on negative numbers is not possible
+        return None
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
--- tests/test_method_name.py
+++ tests/test_method_name.py
@@ -1,12 +1,21 @@
 import pytest
-from method_name import method_name
+
+from method_name.method_name import method_name
+
 
 def test_method_name_positive_factorial():
-  assert method_name(0) == 1
-  assert method_name(1) == 1
-  assert method_name(5) == 120
-  assert method_name(10) == 3628800
+    try:
+        assert method_name(0) == 1
+        assert method_name(1) == 1
+        assert method_name(5) == 120
+        assert method_name(10) == 3628800
+    except Exception as e:
+        pytest.fail(str(e))
+
 
 def test_method_name_negative_factorial():
-  assert method_name(-5) is None
-  assert method_name(-10) is None
+    try:
+        assert method_name(-5) is None
+        assert method_name(-10) is None
+    except Exception as e:
+        pytest.fail(str(e))
